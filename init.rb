d91390305a97611be83bb0f2a8e2910d741abf74
Rakesh C V <rakesh.cv@hotmail.com>
2015-06-18 11:12:47 +1000

    Added application path

---
+++ init.rb
@@ -0,0 +1,19 @@
+=begin
+<<<<<< Movie Finder >>>>>>>
+
+ Launch this Ruby file from the command line
+ to get started.
+
+=end
+
+
+APP_ROOT = File.dirname(__FILE__)
+
+=begin
+ require "#{APP_ROOT}/lib/guide"
+ require File.join(APP_ROOT, 'lib', '')
+ require File.join(APP_ROOT, 'lib', '')
+=end
+
+
+$:.unshift(File.join(APP_ROOT, 'lib'))
