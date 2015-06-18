0fcb5d7dd7b27d709170a62cd1261d8add427898
Rakesh C V <rakesh.cv@hotmail.com>
2015-06-18 13:56:07 +1000

    showing options to the user and act according to it

--- lib/movie_guide.rb
+++ lib/movie_guide.rb
@@ -2,6 +2,12 @@ require 'movie'
 
 class MovieGuide
 
+  @@options = ['list', 'add', 'find', 'quit']
+
+  def self.options
+    @@options
+  end
+
   def initialize(filepath=nil)
     #check for empty filename
 
@@ -28,8 +34,45 @@ class MovieGuide
   def fire!
     introduction
 
+    #present options or menu
+
+    result = nil
+
+    #keep showing options until user hits quit
+    until result == :quit
+      result = respond_to_input(get_input)
+    end
+
     conclusion
   end
+
+
+
+  def get_input
+    user_input = nil
+
+    # Keep asking for user input until we get a valid action
+    until MovieGuide.options.include?(user_input)
+      puts "Options ==>  " << MovieGuide.options.join(", ")
+      user_input = gets.chomp.downcase.strip
+    end
+    user_input
+  end
+
+  def respond_to_input(user_input)
+    case user_input
+    when 'list'
+      puts "listing..."
+    when 'find'
+      puts "finding..."
+    when 'add'
+      add
+      puts "Adding..."
+    when 'quit'
+      return :quit
+    end
+  end
+
 
   def introduction
     puts "\n\n<<< Welcome to the Movie Finder >>>\n\n"
@@ -39,6 +82,10 @@ class MovieGuide
 
   def conclusion
     puts "\n<<< Program Terminated >>>\n\n\n"
+  end
+
+  def add
+
   end
 
 end
